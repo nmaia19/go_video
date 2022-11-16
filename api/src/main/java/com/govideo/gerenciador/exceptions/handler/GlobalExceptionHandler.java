782367099fb8a18e1ca26c21d982f1fd37d04193
aline-fagundes <aline.martinsnapoli@gmail.com>
2022-11-16 15:25:29 -0300

    Ajustes no tratamento de erros de validaçao

--- api/src/main/java/com/govideo/gerenciador/exceptions/handler/GlobalExceptionHandler.java
+++ api/src/main/java/com/govideo/gerenciador/exceptions/handler/GlobalExceptionHandler.java
@@ -7,17 +7,35 @@ import org.springframework.context.MessageSource;
 import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import javax.servlet.http.HttpServletRequest;
 import java.time.Instant;
+import java.util.ArrayList;
+import java.util.List;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
     @Autowired
     private MessageSource messageSource;
+
+    @ResponseStatus(code = HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public List<ErroDeFormResponse> handle(MethodArgumentNotValidException exception) {
+        List<ErroDeFormResponse> errosDTO = new ArrayList<>();
+        List<FieldError> fieldErrors = exception.getBindingResult().getFieldErrors();
+        fieldErrors.forEach(e -> {
+            String mensagem = messageSource.getMessage(e, LocaleContextHolder.getLocale());
+            ErroDeFormResponse erro = new ErroDeFormResponse(e.getField(), mensagem);
+            errosDTO.add(erro);
+        });
+        return errosDTO;
+    }
 
     @ExceptionHandler(Exception.class)
     public final ResponseEntity<ExceptionResponse> allExceptionHandler(Exception ex, HttpServletRequest req) {
